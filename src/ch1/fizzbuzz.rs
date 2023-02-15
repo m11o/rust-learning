74fd621dcc4094904ff79cac5ca37cad4e593f29
m11o <m11o@hey.jp>
2023-02-16 00:11:11 +0900

    implement fizzbuzz

---
+++ src/ch1/fizzbuzz.rs
@@ -0,0 +1,13 @@
+fn main() {
+    for i in 1..51 {
+        if i % 3 == 0 {
+            println!("A")
+        } else if i % 10 == 3 {
+            println!("A")
+        } else if i / 10 == 3 {
+            println!("A")
+        } else {
+            println!("{}", i)
+        }
+    }
+}
